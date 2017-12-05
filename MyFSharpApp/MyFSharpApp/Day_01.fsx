57d8b787b5cbc01381338836e2f435eb61c638f1
Adam Pridmore <adam_pridmore@hotmail.com>
2017-12-05 21:50:24 +0000

    - Solved day 1.

---
+++ MyFSharpApp/MyFSharpApp/Day_01.fsx
@@ -0,0 +1,10 @@
+﻿
+
+let x = "1122"
+
+x.ToCharArray() 
+|> Seq.map (string >> int)
+|> Seq.pairwise
+|> Seq.filter (fun (a,b) -> a = b) 
+|> Seq.map fst
+|> Seq.sum
--- MyFSharpApp/MyFSharpApp/UnitTests.fs
+++
@@ -1,9 +0,0 @@
-﻿module UnitTests
-
-open MyModule
-open NUnit.Framework
-open FsUnit
-
-[<Test>]
-let ``hello world test ``() =
-  helloWorld 123 |> should equal "Hello World 123"
