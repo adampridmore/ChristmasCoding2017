30acf7a98ddcb1078e226bc14b256396add89d80
Grant Crofton <grantac@hotmail.com>
2017-12-06 20:45:18 +0000

    Added grant's solution to day 1

---
+++ grant_fsharp/Solutions.fs
@@ -0,0 +1,20 @@
+module Solutions
+
+let shiftForward n list =
+    (list |> List.skip n)@(list |> List.take n)
+
+let sumRepeats position (input:string)  = 
+    let inputList = input |> List.ofSeq
+
+    inputList
+    |> List.zip (inputList |> shiftForward position)
+    |> List.sumBy( fun (current, next) -> 
+        if current = next then current.ToString() |> int else 1)
+
+let sumFollowingRepeats (input:string) = 
+    input |> sumRepeats 1
+
+let sumHalfwayRepeats (input:string) = 
+    let positionToCheck = (input |> Seq.length) / 2
+    input |> sumRepeats positionToCheck
+    
