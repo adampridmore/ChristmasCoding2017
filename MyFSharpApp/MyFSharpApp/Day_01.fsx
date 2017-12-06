191a6ab021489f2865564d2fc11b9eba411ba8b4
Adam Pridmore <adam_pridmore@hotmail.com>
2017-12-06 19:51:43 +0000

    - Completed day 2 part 2

--- MyFSharpApp/MyFSharpApp/Day_01.fsx
+++ MyFSharpApp/MyFSharpApp/Day_01.fsx
@@ -1,23 +1,30 @@
-﻿
-let rowChecksum (x: string) =
-  let values = x.Split(' ') |> Seq.map (string >> int)
-
-  (values |> Seq.max) - (values |> Seq.min)
-
-
-let x = "5 1 9 5"
+﻿//let x = "5 9 2 8"
+//let x = "9 4 7 3"
+let x = "3 8 6 5"
 
 
 let split (text:string) = 
   let delimiters = [|",";"\n";System.Environment.NewLine|]
   text.Split(delimiters, System.StringSplitOptions.RemoveEmptyEntries)
 
-let text = "5 1 9 5
-7 5 3
-2 4 6 8"
+let rowChecksum(text:string) = 
 
-text 
-|> split
-|> Seq.map rowChecksum
-|> Seq.sum
+  let values = text.Split(' ') |> Seq.map (string >> int)
+
+  let OrderedTuple (a,b) = 
+    if a > b then (a,b)
+    else (b,a)
+  
+  let processThing (values:seq<int>, value:int) =
+    values 
+    |> Seq.map(fun x -> OrderedTuple(x,value))
+    |> Seq.map(fun (a,b) -> (a / b), a % b = 0)
+    |> Seq.filter snd
+    |> Seq.map fst
+    |> Seq.tryHead
+
+  values 
+  |> Seq.mapi (fun i v -> values |> Seq.skip (i + 1) |> (fun x -> (x, v)))
+  |> Seq.choose processThing
+  |> Seq.head
 
