777d6482532cae3b5d7a01ae02a40d594d7544d2
Adam Pridmore <adam_pridmore@hotmail.com>
2017-12-06 19:00:53 +0000

    - Completed day 1 part 2

--- MyFSharpApp/MyFSharpApp/Day_01.fsx
+++ MyFSharpApp/MyFSharpApp/Day_01.fsx
@@ -1,10 +1,15 @@
 ﻿
 
-let x = "1122"
+let x = "1212"
 
-x.ToCharArray() 
-|> Seq.map (string >> int)
-|> Seq.pairwise
-|> Seq.filter (fun (a,b) -> a = b) 
-|> Seq.map fst
+let values = x.ToCharArray() |> Seq.map (string >> int) |> Seq.toList
+
+let getOtherValue i =
+  (i + (values.Length/2)) % values.Length
+  
+values
+|> Seq.mapi (fun i v -> (i,v) ) 
+|> Seq.filter(fun (i, v) -> values.[i |> getOtherValue] = v)
+|> Seq.map snd
 |> Seq.sum
+
